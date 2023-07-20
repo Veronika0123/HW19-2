b86c7659601be85aa851ac3c8d6d94ad7d96ba75
Veronika0123 <veronikamokrii@gmail.com>
2023-07-20 23:34:28 +0300

    /

---
+++ app.js
@@ -0,0 +1,8 @@
+let year = +prompt('Какой сейчас год');
+if (year % 4 == 0 || year % 100 != 0 && year % 400 == 0){
+    console.log(` ${year} высокосный`);
+}
+
+else{
+    console.log(` ${year} невысокосный`);
+}
